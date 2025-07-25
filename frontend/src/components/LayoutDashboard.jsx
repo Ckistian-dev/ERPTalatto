c27ee8f6dcf1129f50079e789ea02b637d4deabc
Ckistian-dev <crisgkist@gmail.com>
2025-07-25 10:58:22 -0300

    Resolução do erro do Layout

--- frontend/src/components/LayoutDashboard.jsx
+++ frontend/src/components/LayoutDashboard.jsx
@@ -81,7 +81,7 @@ export default function LayoutDashboard() {
                 <SidebarItem icone={<FaUserFriends />} titulo="Cadastros" aberto={menuAberto === 'geral'} toggle={() => toggleMenu('geral')} submenu={["Geral", "Produtos", "Usuários", "Embalagem"]} sidebarAberta={sidebarAberta} links={["/cadastros/novo", "/produtos/novo", "usuario/novo", "embalagem/novo"]} location={location} />
             )}
             {verificarPermissao(usuario, ['admin', 'vendedor', 'financeiro', 'visitante']) && (
-                <SidebarItem icone={<FaBoxOpen />} titulo="Listagem" aberto={menuAberto === 'listagem'} toggle={() => toggleMenu('listagem')} submenu={["Cadastros", "Produtos", "Usuários", "Embalagens"]} sidebarAberta={sidebarAberta} links={["/cadastros", "/produtos", "/usuario", "/embalagem"]} location={location} exclude={['/cadastros/novo', '/produtos/novo', '/embalagem/novo']} />
+                <SidebarItem icone={<FaBoxOpen />} titulo="Listagem" aberto={menuAberto === 'listagem'} toggle={() => toggleMenu('listagem')} submenu={["Cadastros", "Produtos", "Usuários", "Embalagens"]} sidebarAberta={sidebarAberta} links={["/cadastros", "/produtos", "/usuario", "/embalagem"]} location={location} exclude={['/cadastros/novo', '/produtos/novo', "usuario/novo",  '/embalagem/novo']} />
             )}
             {verificarPermissao(usuario, ['admin', 'vendedor', 'visitante']) && (
                 <SidebarItem icone={<FaFileInvoiceDollar />} titulo="Orçamentos" aberto={menuAberto === 'orcamentos'} toggle={() => toggleMenu('orcamentos')} submenu={["Novo Orçamento", "Histórico"]} sidebarAberta={sidebarAberta} links={["/orcamentos/novo", "/orcamentos"]} location={location} />
